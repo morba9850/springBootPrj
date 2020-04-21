409664f168d986d2522e251a5fe181700178ea6f
morba9850 <morba9850@naver.com>
2020-04-21 22:49:10 +0900

    gitignore 등록

---
+++ src/main/java/com/bhs/springboot/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.bhs.springboot.web;
+
+import com.bhs.springboot.config.auth.dto.SessionUser;
+import com.bhs.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/bhs/springboot/web/PostsApiController.java
@@ -0,0 +1,26 @@
+package com.bhs.springboot.web;
+
+import com.bhs.springboot.service.PostsService;
+import com.bhs.springboot.web.dto.PostsSaveRequestDto;
+import com.bhs.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto
+                     requestDto) {
+
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+}
